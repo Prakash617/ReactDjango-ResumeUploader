71b39274ee342a1b4a4d349ca026bc4c1254a70d
prakash <prakashthapa617@gmail.com>
2022-07-06 14:25:28 +0545

    getData

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+   
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
--- frontend/src/Components/Tables/Table.tsx
+++ frontend/src/Components/Tables/Table.tsx
@@ -1,67 +1,67 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+  data: any
+}
 
 const Table = (props: Props) => {
+  const { data } = props
+  console.log(`this is data : ${data}`)
   return (
     <div>
 
-<table className="table-auto">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>DOB</th>
-      <th>State</th>
-      <th>Gender</th>
-      <th>Location</th>
-      <th>Avatar</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones</td>
-      <td>prakash@gmail.com</td>
-      <td>1961</td>
-      <td>ktm</td>
-      <td>male</td>
-      <td>kathmandu</td>
-      <td>photo</td>
-      <td className='flex justify-between'>
-        <div className='text-green-500 text-2xl cursor-pointer'>E</div>
-        <div className='text-red-500 text-2xl cursor-pointer'>D</div>
-      </td>
-    </tr>
-    <tr>
-      <td>The Sliding Mr. Bones</td>
-      <td>prakash@gmail.com</td>
-      <td>1961</td>
-      <td>ktm</td>
-      <td>male</td>
-      <td>kathmandu</td>
-      <td>photo</td>
-      <td className='flex justify-between'>
-        <div className='text-green-500 text-2xl cursor-pointer'>E</div>
-        <div className='text-red-500 text-2xl cursor-pointer'>D</div>
-      </td>
-    </tr>
-    <tr>
-      <td>The Sliding Mr. Bones</td>
-      <td>prakash@gmail.com</td>
-      <td>1961</td>
-      <td>ktm</td>
-      <td>male</td>
-      <td>kathmandu</td>
-      <td>photo</td>
-      <td className='flex justify-between'>
-        <div className='text-green-500 text-2xl cursor-pointer'>E</div>
-        <div className='text-red-500 text-2xl cursor-pointer'>D</div>
-      </td>
-    </tr>
-   
-  </tbody>
-</table>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>DOB</th>
+            <th>State</th>
+           
+            <th>Location</th>
+            <th>Avatar</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data?.data.map((data: any) => {
+              return (
+
+                <tr key={data.name}>
+                  <td>{data.name}</td>
+                  <td>{data.email}</td>
+                  <td>{data.DOB}</td>
+                  <td>{data.state}</td>
+                  <td>{data.job_location}</td>
+                  <td><img src={`${data.img}`} className='rounded-full h-8 w-8' alt="" /></td>
+                 
+                  <td className='flex justify-between'>
+                    <div className='text-green-500 text-2xl cursor-pointer'>E</div>
+                    <div className='text-red-500 text-2xl cursor-pointer'>D</div>
+                  </td>
+                </tr>
+              )
+            })
+          }
+          
+          {/* <tr>
+            <td>The Sliding Mr. Bones</td>
+            <td>prakash@gmail.com</td>
+            <td>1961</td>
+            <td>ktm</td>
+           
+            <td>kathmandu</td>
+            <td>photo</td>
+            <td className='flex justify-between'>
+              <div className='text-green-500 text-2xl cursor-pointer'>E</div>
+              <div className='text-red-500 text-2xl cursor-pointer'>D</div>
+            </td>
+          </tr> */}
+        
+
+        </tbody>
+      </table>
     </div>
   )
 }
